6005e20ef9bbc3346e61cc6d01bb31e27824f8ef
Reik Braune <Reik.Braune@me.com>
2025-10-15 13:38:32 +0200

    Delete saw_tricycle directory

--- saw_tricycle/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Saw Tricycle control package."""
-
-__all__ = [
-    "config",
-    "control",
-    "gamepad",
-    "state",
-]
--- saw_tricycle/__main__.py
+++
@@ -1,7 +0,0 @@
-"""Entry point for ``python -m saw_tricycle``."""
-
-from .control import main
-
-
-if __name__ == "__main__":  # pragma: no cover
-    main()
